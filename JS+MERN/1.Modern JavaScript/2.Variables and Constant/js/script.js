89509d3edf4882bb53076b984a91eb9daf3726bf
Tapantor Das <109220196+Cpsbd66@users.noreply.github.com>
2023-10-07 13:11:36 +0600

    Add files via upload

---
+++ JS+MERN/1.Modern JavaScript/2.Variables and Constant/js/script.js
@@ -0,0 +1,29 @@
+//variable 
+
+var x;
+let y;
+
+x = 45;
+y = "Cpsbd66";
+
+console.log(x);
+console.log(y);
+
+
+var a = 20;
+var b = 10;
+var c = a+ b;
+
+console.log(a+b)
+
+/*
+variables are case sensitive
+cant be started with numbers
+*/
+
+
+//constant and keywords
+
+const pi = 3.1415 //value of constant can't be changed 
+
+//keywords => used for javascirpt, cant be declared as constant or variable
